74ae761f3b29b46dff7f76ce93f8198057a1fcca
oconnormichael <76404974+oconnormichael@users.noreply.github.com>
2021-01-29 10:02:21 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var firet, firetImage, water, waterImage, waterGroup, house, houseImage, flame, flameImage, flameGroup, flame2, flame2Image, flameGroup2, flame3, flame3Image, flameGroup3;
 var gameState = "pause";
-var item1, score, miss, high, buy, buyImage;
+var item1, score, miss, high, buy, buyImage, item2, buy2, buy2Image;
 var gameState = "play";
-var shop, shopImage, timeball;
+var shop, shopImage, timeball, timeball2;
 
 function preload() {
   firetImage = loadImage("firetruck.png");
@@ -13,14 +13,16 @@ function preload() {
   flame3Image = loadImage("fire3.png");
   shopImage = loadImage("shop.png");
   buyImage = loadImage("buy.png");
+  buy2Image=loadImage("buy2.png");
 }
 
 
 function setup() {
   createCanvas(600, 600);
   timeball = createSprite(0, 0, 10, 10);
+  timeball2= createSprite(0,0,10,10);
 
-
+  
 
   house = createSprite(300, 300, 600, 600);
   house.addImage(houseImage);
@@ -41,16 +43,24 @@ function setup() {
   buy = createSprite(124, 104, 109, 112);
   buy.addImage(buyImage);
   buy.visible = false;
+  
+  buy2=createSprite(267,104,109,112);
+  buy2.addImage(buy2Image);
+  buy2.visible=false;
 
   flameGroup = new Group();
   waterGroup = new Group();
   water2Group = new Group();
   flameGroup2 = new Group();
   flameGroup3 = new Group();
+  flameGroup1_1= new Group();
+  flameGroup1_2= new Group();
+  flameGroup1_3= new Group();
   score = 0;
   miss = 0;
   high = 0;
   item1 = 0;
+  item2=0;
 }
 
 function draw() {
@@ -64,8 +74,8 @@ function draw() {
 
     shop.visible = false;
     buy.visible = false;
-
-    if (keyDown("B")) {
+buy2.visible = false;
+    if (keyDown("S")) {
       gameState = "pause";
     }
     if (keyDown("LEFT")) {
@@ -79,6 +89,9 @@ function draw() {
     flame();
     flame2();
     flame3();
+    flame1_1()
+    flame1_2()
+    flame1_3()
     if (flameGroup.isTouching(waterGroup)) {
       flameGroup.destroyEach();
       waterGroup.destroyEach();
@@ -139,10 +152,73 @@ function draw() {
 
     }
 
+  if (flameGroup1_1.isTouching(waterGroup)) {
+      flameGroup1_1.destroyEach();
+      waterGroup.destroyEach();
+      score = score + 2;
+    }
+
+    if (flameGroup1_2.isTouching(waterGroup)) {
+      flameGroup1_2.destroyEach();
+      waterGroup.destroyEach();
+      score = score + 6;
+    }
+    if (flameGroup1_3.isTouching(waterGroup)) {
+      flameGroup1_3.destroyEach();
+      waterGroup.destroyEach();
+      score = score + 10;
+    }
+    if (flameGroup1_1.isTouching(water2Group)) {
+      flameGroup1_1.destroyEach();
+      water2Group.destroyEach();
+      score = score + 2;
+    }
+
+    if (flameGroup1_2.isTouching(water2Group)) {
+      flameGroup1_2.destroyEach();
+      water2Group.destroyEach();
+      score = score + 6;
+    }
+    if (flameGroup1_3.isTouching(water2Group)) {
+      flameGroup1_3.destroyEach();
+      water2Group.destroyEach();
+      score = score + 10;
+    }
+
+    if (flameGroup1_1.isTouching(bar)) {
+      if (high < score) {
+        high = score;
+      }
+      score = score = 0;
+      flameGroup1_1.destroyEach();
+      miss = miss + 1;
+    }
+    if (flameGroup1_2.isTouching(bar)) {
+      if (high < score) {
+        high = score;
+      }
+      score = score = 0;
+      flameGroup1_2.destroyEach();
+      miss = miss + 1;
+
+    }
+    if (flameGroup1_3.isTouching(bar)) {
+      if (high < score) {
+        high = score;
+      }
+      score = score = 0;
+      flameGroup1_3.destroyEach();
+      miss = miss + 1;
+
+    }
+    
   }
   if (gameState === "pause") {
     if (timeball > 1) {
       buy.visible = true;
+    }
+    if (timeball2 > 1) {
+      buy2.visible = true;
     }
     shop.visible = true;
 
@@ -153,6 +229,13 @@ function draw() {
       timeball = timeball.x + 10;
 
     }
+    
+    if (keyDown("B") && (score >= 30) && (timeball2.x < 1)) {
+      item2 = item2 + 1;
+      score = score - 30;
+      timeball2 = timeball2.x + 10;
+
+    }
 
     if (keyDown("C")) {
       gameState = "play";
@@ -161,7 +244,11 @@ function draw() {
     flameGroup.destroyEach();
     flameGroup2.destroyEach();
     flameGroup3.destroyEach();
-
+    flameGroup1_1.destroyEach();
+    flameGroup1_2.destroyEach();
+    flameGroup1_3.destroyEach();
+  waterGroup.destroyEach();
+    water2Group.destroyEach();
   }
 
   drawSprites();
@@ -179,7 +266,7 @@ function draw() {
 
 
 function flame() {
-  if (frameCount % 50 === 0) {
+ if(item2===0){ if (frameCount % 50 === 0) {
     var flame = createSprite(200, -140)
     flame.x = Math.round(random(100, 400))
     flame.addImage(flameImage)
@@ -194,9 +281,9 @@ function flame() {
     flameGroup.add(flame);
   }
 }
-
+}
 function flame2() {
-  if (score > 10) {
+  if(item2===0){if (score > 10) {
     if (frameCount % 300 === 0) {
       var flame2 = createSprite(200, -140)
       flame2.x = Math.round(random(100, 400))
@@ -213,9 +300,9 @@ function flame2() {
     }
   }
 }
-
+}
 function flame3() {
-  if (score > 25) {
+ if(item2===0){ if (score > 25) {
     if (frameCount % 500 === 0) {
       var flame3 = createSprite(200, -140)
       flame3.x = Math.round(random(100, 400))
@@ -232,7 +319,63 @@ function flame3() {
     }
   }
 }
+}
 
+function flame1_1() {
+  if(item2===1){if (frameCount % 50 === 0) {
+    var flame1_1 = createSprite(200, -140)
+    flame1_1.x = Math.round(random(100, 400))
+    flame1_1.addImage(flameImage)
+    flame1_1.scale = 0.08;
+    flame1_1.velocityY = 10;
+    flame1_1.lifetime = 900;
+    flame1_1.setCollider("circle", 0, 150, 240);
+    flame1_1.debug = false;
+    if (score > 50) {
+      flame1_1.velocityY = 15;
+    }
+    flameGroup1_1.add(flame1_1);
+  }
+}
+}
+function flame1_2() {
+   if(item2===1){if (score > 10) {
+    if (frameCount % 300 === 0) {
+      var flame1_2 = createSprite(200, -140)
+      flame1_2.x = Math.round(random(100, 400))
+      flame1_2.addImage(flame2Image)
+      flame1_2.scale = 0.08;
+      flame1_2.velocityY = 15;
+      flame1_2.lifetime = 900;
+      flame1_2.setCollider("circle", 0, 150, 240);
+      flame1_2.debug = false;
+      if (score > 50) {
+        flame1_2.velocityY = 20;
+      }
+      flameGroup1_2.add(flame1_2);
+    }
+  }
+}
+}
+function flame1_3() {
+  if(item2===1){ if (score > 25) {
+    if (frameCount % 500 === 0) {
+      var flame1_3 = createSprite(200, -140)
+      flame1_3.x = Math.round(random(100, 400))
+      flame1_3.addImage(flame3Image)
+      flame1_3.scale = 0.08;
+      flame1_3.velocityY = 20;
+      flame1_3.lifetime = 900;
+      flame1_3.setCollider("circle", 0, 150, 240);
+      flame1_3.debug = false;
+      if (score > 50) {
+        flame1_3.velocityY = 25;
+      }
+      flameGroup1_3.add(flame1_3);
+    }
+  }
+}
+}
 function water() {
   if (item1 === 0) {
     if (keyDown("space")) {
